8392044aa9b98eccc3611918bbd6fc0c52e61a58
otiagoluz <tiagoluzgpr@gmail.com>
2020-06-27 08:43:53 -0400

    fix pagaeable

--- src/main/java/me/tiagoluz/forum/controller/TopicoController.java
+++ src/main/java/me/tiagoluz/forum/controller/TopicoController.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -34,10 +36,8 @@ public class TopicoController {
   @GetMapping
   public Page<TopicoDto> lista(
   @RequestParam(required = false) String nomeCurso,
-  @RequestParam int page,
-  @RequestParam int amt) {
+  @PageableDefault(sort = "id", page = 0, size = 10, direction = Sort.Direction.ASC) Pageable pageable) {
 
-    Pageable pageable = PageRequest.of(page, amt);
     System.out.println(nomeCurso);
     if (nomeCurso == null) {
       Page<Topico> topicos = topicoRepository.findAll(pageable);
